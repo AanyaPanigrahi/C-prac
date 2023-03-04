e9856a8f285115d43aecce0a957bb5ef2ba9cf40
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-03-04 23:46:57 +0530

    Add files via upload

---
+++ practice8.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	/*
+	LOOP (repetition)
+	1. while
+	while(exp) test => true/false
+	{
+	    statements;
+	}
+	*/
+	int a=1,n;
+	while (a<=11)
+	{
+		printf("\n Hello ");
+		a=a+2*1;
+	}
+	printf("\n %d",a);
+	
+	// making a table
+	//while (i<=10)
+	
+	// for (exp1);(exp2);(exp3)
+	return 0;
+}
