63ee048c133988c73ef11973fbba04b9d9d30ff6
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-03-26 22:42:47 +0530

    Add files via upload

---
+++ practice25.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define SIZE 5
+int main(){
+	int Arr[SIZE]={10,20,30,40,50};
+	int i;
+	// Print(Display) array elements(value)
+	for (i=0;i<SIZE;i++)
+	{
+		printf("\nArr[%d]=%d",i,Arr[i]);
+	}
+}
