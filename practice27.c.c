abcaf6eb194f4fc4d9df2a13be8d8a7eac5590a4
Aanya Panigrahi <114793986+AanyaPanigrahi@users.noreply.github.com>
2023-04-08 23:24:32 +0530

    Add files via upload

---
+++ practice27.c.c
@@ -0,0 +1,25 @@
+//  #include <stdio.h>
+//  int main(){
+//	int arr[100]; // static memory allocation
+//	int i;
+//	int value;
+//	printf("\nEnter the size of array = ")
+//	scanf("%d",&value);
+//	printf("\nElement[%d]=");
+//	scanf("%d",&value);
+//	// shift the value to next position
+//	for (i=size;i>0;i--)
+//	{
+//		arr[i]=arr[i-1];
+//	}
+//	arr[0]=value;
+//	size=size+1;
+//	NEW_LINE;
+//	printf("\nAfter Insertion\n");
+//	// Traversing
+//	for(i=0;i<size;i++)
+//	{
+//		printf("\nElement[%d]=%d",i+1,arr[i]);
+//	}
+//}
+
